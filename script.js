92a3c323561fedee27ac4c4dca3244e4a89bcc27
atrbyg24 <artwan520@gmail.com>
2025-01-30 15:29:07 -0500

    Create Book object constructor and addBookToLibrary function

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+
+}
+
+function addBookToLibrary(title, author,pages,readStatus) {
+    let newBook = new Book(title,author,pages,readStatus);
+    myLibrary.push(newBook);
+}
