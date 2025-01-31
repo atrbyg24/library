7cb0241ca2e20f10053102bbeefe004cd87ea55a
atrbyg24 <artwan520@gmail.com>
2025-01-31 14:54:52 -0500

    Write js function to add books to table

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function Book(title, author, pages, readStatus) {
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.readStatus ? "Read" : "Not Read"}`;
+    }
+    this.toggleRead = function () {
+        this.readStatus = !this.readStatus;
+        return this.readStatus;
+    }
 }
 
 function addBookToLibrary(title, author,pages,readStatus) {
@@ -12,6 +19,35 @@ function addBookToLibrary(title, author,pages,readStatus) {
     myLibrary.push(newBook);
 }
 
-function displayBooks() {
+function displayBooks(myLibrary) {
+    const tableBody = document.querySelector(".book-list");
+    tableBody.replaceChildren();
+    
+    for (const [index, book] of myLibrary.entries()) {
+        const tableBody = document.querySelector(".book-list");
+        const bookRow = document.createElement("tr");
+        const bookTitle = document.createElement("td");
+        bookTitle.textContent = book.title;
+        const bookAuthor = document.createElement("td");
+        bookAuthor.textContent = book.author;
+        const bookPages = document.createElement("td");
+        bookPages.textContent = book.pages;
+        const bookReadStatus = document.createElement("td");
+        bookReadStatus.textContent = book.readStatus ? "Read" : "Not Read";
 
+        bookRow.append(bookTitle);
+        bookRow.append(bookAuthor);
+        bookRow.append(bookPages);
+        bookRow.append(bookReadStatus);
+
+        tableBody.append(bookRow);
+    }
 }
+
+addBookToLibrary("To Kill a MockingBird","Harper E. Lee",234,true);
+addBookToLibrary("One Fish, Two Fish","Dr. Seuss",40,true);
+addBookToLibrary("To Kill a MockingBird","Harper E. Lee",234,true);
+addBookToLibrary("One Fish, Two Fish","Dr. Seuss",40,true);
+addBookToLibrary("To Kill a MockingBird","Harper E. Lee",234,true);
+addBookToLibrary("One Fish, Two Fish","Dr. Seuss",40,true);
+displayBooks(myLibrary);
